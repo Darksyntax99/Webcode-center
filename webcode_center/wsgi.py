8b55c125b4446ec0d86cda692d5d38e10e084b92
Darksyntax99 <darksyntax99@gmail.com>
2026-08-21 17:30:55 +0200

    Setup django project courses app

---
+++ webcode_center/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for webcode_center project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webcode_center.settings')
+
+application = get_wsgi_application()
