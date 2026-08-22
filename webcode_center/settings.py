b2195e9842ddcfe533376bb1af9a20333f60577c
Darksyntax99 <darksyntax99@gmail.com>
2026-08-22 19:48:30 +0200

    add template and bootstrap

--- webcode_center/settings.py
+++ webcode_center/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'webcode_center.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
