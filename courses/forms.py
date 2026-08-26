861f4fe085c61974f3a429a0a741d0031e43107e
Darksyntax99 <darksyntax99@gmail.com>
2026-08-26 14:55:59 +0200

    add forms crud

---
+++ courses/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import Course
+
+# Course form
+class CourseForm(forms.ModelForm):
+    class Meta:
+        model = Course
+        fields = ['title', 'description', 'price']
+        
