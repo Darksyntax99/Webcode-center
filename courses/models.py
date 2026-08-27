84c7dd1d77a665e60bdd1ecba970f6b5f64995cc
Darksyntax99 <darksyntax99@gmail.com>
2026-08-27 21:12:45 +0200

    fix edit rating

--- courses/models.py
+++ courses/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator 
 
 # Course model.
 class Course(models.Model):
@@ -43,7 +44,12 @@ class Review(models.Model):
 
     content = models.TextField()
 
-    rating = models.PositiveIntegerField()
+    rating = models.PositiveIntegerField(
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(5)
+        ]
+    )
 
     def __str__(self):
         return f'{self.user.username} - {self.course.title}'
