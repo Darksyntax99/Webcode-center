38dfb90c2d74d3179c99782c328adf02d7855275
Darksyntax99 <darksyntax99@gmail.com>
2026-09-01 16:39:02 +0200

    deployment the project

--- webcode_center/settings.py
+++ webcode_center/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-idswz!6j++$a9(l6hze*888fp8gosg!nyi(4g22qw=96!!qo@c'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -134,6 +134,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [BASE_DIR / 'static',]
 
